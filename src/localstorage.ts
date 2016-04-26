75c52d0dd3e811444b9d3ebae3e06b510a3a2e4a
SirDarquan <sirdarquan@gmail.com>
2016-04-26 12:33:13 -0500

    added jsdoc comments to test generation

--- src/localstorage.ts
+++ src/localstorage.ts
@@ -2,6 +2,9 @@ import {Inject, Injectable, Optional, OpaqueToken, NgZone} from "angular2/core";
 import {BaseStorage, StorageOptions, STORAGE_OPTIONS} from "./basestorage";
 
 export let LOCAL_STORAGE_OBJECT = new OpaqueToken("localstorage");
+/**
+ * Some information about LocalStorage
+ */
 @Injectable()
 export class LocalStorage extends BaseStorage {
 	constructor(ngZone: NgZone, @Inject(LOCAL_STORAGE_OBJECT) storage: Storage, @Inject(STORAGE_OPTIONS) @Optional() options?: StorageOptions) {
